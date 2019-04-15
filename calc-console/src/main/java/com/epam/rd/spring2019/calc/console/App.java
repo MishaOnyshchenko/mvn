486ceb3cdc9d12dd552348c49335373d21eca8ad
misha.onyshchenko <misha.onyshchenko@gmail.com>
2019-04-16 01:01:23 +0300

    release 1.0

--- calc-console/src/main/java/com/epam/rd/spring2019/calc/console/App.java
+++ calc-console/src/main/java/com/epam/rd/spring2019/calc/console/App.java
@@ -1,31 +1,32 @@
 package com.epam.rd.spring2019.calc.console;
 
 import com.epam.rd.spring2019.calc.core.CalcImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 
-/*
-При запуске программы, через переменные окружения будут передаваться следующие аргументы:
-number1 number2 operator
-
-Примеры операндов:
-100 -100 +
-
-Программа должна вывести в консоль следующее сообщение и завершиться:
-number1=100 number2=-100 operator=+ result=0
-* */
-
 public class App {
-    public static void main(String[] args) {
+    private final static Logger logger = LoggerFactory.getLogger(App.class);
 
-        System.out.println(Arrays.toString(args));
-        System.out.println(args.length);
+    public static void main(String[] args) {
+        logger.info("Application started");
+        logger.info("Name: calc");
+        logger.info("Version: 1.0");
+        logger.info("args length: " + args.length);
+        logger.info("args value: " + Arrays.toString(args));
 
         if (args.length == 3) {
             CalcImpl calcImpl = new CalcImpl();
+            logger.info("Calling calcImpl.calculate(args)");
             calcImpl.calculate(args);
+            logger.info("calcImpl.calculate(args) passed");
         } else {
-            System.err.println("Invalid input data! Correct format: number1 number2 operator (+ - x /)");
+            logger.info("Application interrupted. Invalid args.length");
+            logger.info("Correct input format: number1 number2 operator(+ - x /)");
+            logger.warn("Current value: " + Arrays.toString(args));
+            logger.error("Invalid input data: " + Arrays.toString(args));
         }
+        logger.info("Application calc 1.0 finished");
     }
 }
--- calc-core/src/main/java/com/epam/rd/spring2019/calc/core/CalcImpl.java
+++ calc-core/src/main/java/com/epam/rd/spring2019/calc/core/CalcImpl.java
@@ -1,29 +1,38 @@
 package com.epam.rd.spring2019.calc.core;
 
 import com.epam.rd.spring2019.calc.interfaces.Calc;
+import org.slf4j.*;
 
 public class CalcImpl implements Calc {
+    private static final Logger logger = LoggerFactory.getLogger(CalcImpl.class);
+
     @Override
     public double addition(double a, double b) {
+        logger.info("addition runs");
         return a + b;
     }
 
     @Override
     public double subtraction(double a, double b) {
+        logger.info("subtraction runs");
         return a - b;
     }
 
     @Override
     public double multiplication(double a, double b) {
+        logger.info("multiplication runs");
         return a * b;
     }
 
     @Override
     public double division(double a, double b) {
+        logger.info("division runs");
         return a / b;
     }
 
     public void calculate(String[] args) {
+        logger.info("calculate runs");
+
         double a = 0;
         double b = 0;
         String operator = args[2].toLowerCase();
@@ -34,7 +43,9 @@ public class CalcImpl implements Calc {
             b = Double.valueOf(args[1]);
         }
         catch (NumberFormatException e){
-            System.out.println("Invalid input operand! Allowed only arabic numerals!");
+            logger.info("Application interrupted. Allowed only arabic numerals!");
+            logger.warn("Invalid input operand!");
+            logger.error("Invalid input operand(s). Received value: a=" + args[0] + ", b=" + args[1]);
             return;
         }
 
@@ -52,9 +63,11 @@ public class CalcImpl implements Calc {
                 result = division(a, b);
                 break;
             default:
-                System.out.println("Invalid operator! Correct format: + - x /");
+                logger.warn("Input operator: " + operator);
+                logger.error("Invalid operator. Correct format: + - x /");
                 break;
         }
+
         System.out.println("number1=" + a + " number2=" + b + " operator=" + operator + " result=" + result);
     }
 }
